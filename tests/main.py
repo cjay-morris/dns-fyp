98c3a9725b1c414f5d46a08963583229f7c18697
cjay-morris <91533218+cjay-morris@users.noreply.github.com>
2024-01-23 12:14:33 +0000

    feat: add CNAME record support, add tests folder

---
+++ tests/main.py
@@ -0,0 +1,36 @@
+import yaml
+import os
+
+currentAttributes = {"Type", "TTL", "Value"}
+
+# helpers
+
+def hasAttribute(obj, attribute):
+    for key in obj:
+        if key == attribute:
+            return True
+    return False
+
+def getMapping():
+    mapping = {}
+    for zoneName in os.listdir("../zones"):
+        for record in os.listdir("../zones/" + zoneName):
+            recordObj = yaml.safe_load(open("../zones/" + zoneName + "/" + record))
+            mapping[zoneName] = recordObj
+    return mapping
+
+# tests
+
+def test_hasAttribute():
+    mapping = getMapping()
+    for zoneName in mapping:
+        recordObj = mapping[zoneName]
+        for attribute in currentAttributes:
+            try:
+                assert hasAttribute(recordObj, attribute)
+            except AssertionError:
+                print("Record " + zoneName + " has no attribute '" + attribute + "'")
+
+# run tests
+
+test_hasAttribute()
